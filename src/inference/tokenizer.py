83512da0874ab67613ae53494ec8134a14d7ff57
mohamedrafat9 <raftmhmaa@gmail.com>
2026-08-19 16:27:54 +0300

    update tokenizer.py

--- src/inference/tokenizer.py
+++ src/inference/tokenizer.py
@@ -1,11 +1,25 @@
-def tokenize(text,tokenizer,device):
-    ids = tokenizer(text, return_tensors="pt").to(device)
-    return ids
+# def tokenize(text,tokenizer,device):
+#     ids = tokenizer(text, return_tensors="pt").to(device)
+#     return ids
 
-# prompt = tokenizer.apply_chat_template(
-#     messages,
-#     tokenize=False,
-#     add_generation_prompt=True
-# )
+# # prompt = tokenizer.apply_chat_template(
+# #     messages,
+# #     tokenize=False,
+# #     add_generation_prompt=True
+# # )
 
-# print(prompt)
+# # print(prompt)
+def tokenize(messages, tokenizer, device):
+
+    prompt = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+
+    inputs = tokenizer(
+        prompt,
+        return_tensors="pt"
+    ).to(device)
+
+    return inputs
