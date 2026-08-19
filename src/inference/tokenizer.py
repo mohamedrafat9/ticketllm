b53fc74298274573811fbc3c46b68bbae6f9959c
mohamedrafat9 <raftmhmaa@gmail.com>
2026-08-19 03:31:49 +0300

    update tokenizer.py to match the model

--- src/inference/tokenizer.py
+++ src/inference/tokenizer.py
@@ -1,3 +1,11 @@
 def tokenize(text,tokenizer,device):
     ids = tokenizer(text, return_tensors="pt").to(device)
     return ids
+
+# prompt = tokenizer.apply_chat_template(
+#     messages,
+#     tokenize=False,
+#     add_generation_prompt=True
+# )
+
+# print(prompt)
