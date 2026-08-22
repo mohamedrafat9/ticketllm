3b7fb3f66ac027ad63fb81973d83e31654c7b66b
mohamedrafat9 <raftmhmaa@gmail.com>
2026-08-23 00:02:04 +0300

    و

--- src/inference/generate.py
+++ src/inference/generate.py
@@ -22,42 +22,56 @@ def generate_text(
     input_ids = inputs["input_ids"]
     attention_mask = inputs["attention_mask"]
 
-    past_key_values = None
+    generated_tokens = []
 
     with torch.inference_mode():
 
-        
+        # First forward pass
         outputs = model(
             input_ids=input_ids,
             attention_mask=attention_mask,
             use_cache=True
         )
 
+        past_key_values = outputs.past_key_values
+
         next_token_logits = outputs.logits[:, -1, :]
 
+        # Temperature
         next_token_logits = apply_temperature(
             next_token_logits,
             temperature
         )
 
+        # Top-k
         if top_k is not None:
             next_token_logits = apply_top_k(
                 next_token_logits,
                 top_k
             )
 
+        # Select token
         next_token = select_next_token(
             next_token_logits,
-            temperature
+            1.0
         )
 
-        past_key_values = outputs.past_key_values
+        generated_tokens.append(next_token)
 
-        generated_tokens = [next_token]
+        # Stop if EOS
+        if (
+            tokenizer.eos_token_id is not None
+            and next_token.item() == tokenizer.eos_token_id
+        ):
+            return tokenizer.decode(
+                next_token[0],
+                skip_special_tokens=True
+            )
 
-        
+        # Generate remaining tokens
         for _ in range(max_new_tokens - 1):
 
+            # Only pass the newly generated token
             input_ids = next_token
 
             attention_mask = torch.cat(
@@ -79,6 +93,8 @@ def generate_text(
                 use_cache=True
             )
 
+            past_key_values = outputs.past_key_values
+
             next_token_logits = outputs.logits[:, -1, :]
 
             next_token_logits = apply_temperature(
@@ -94,10 +110,8 @@ def generate_text(
 
             next_token = select_next_token(
                 next_token_logits,
-                temperature
+                1.0
             )
-
-            past_key_values = outputs.past_key_values
 
             generated_tokens.append(next_token)
 
@@ -107,43 +121,13 @@ def generate_text(
             ):
                 break
 
-    generated_tokens = torch.cat(generated_tokens, dim=-1)
-
-    output_ids = torch.cat(
-        [inputs["input_ids"], generated_tokens],
+    # Decode ONLY generated tokens
+    generated_ids = torch.cat(
+        generated_tokens,
         dim=-1
     )
 
     return tokenizer.decode(
-        output_ids[0],
+        generated_ids[0],
         skip_special_tokens=True
     )
-
-
-# from src.inference.decoding import select_next_token
-# from src.inference.decoding import apply_temperature, apply_top_k
-# from src.inference.load_model import get_model_device
-# from src.inference.tokenizer import tokenize
-# import torch
-
-
-# def generate_text(model, tokenizer, prompt, max_length=50, temperature=1.0,top_p=None, top_k=None,max_new_tokens=50):
-#     device = get_model_device(model)
-#     inputs = tokenize(prompt, tokenizer, device)
-#     input_ids = inputs['input_ids']
-#     attention_mask = inputs['attention_mask']
-#     for _ in range(max_new_tokens):
-#         outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-#         next_token_logits = outputs.logits[:, -1, :]
-#         next_token_logits = apply_temperature(next_token_logits, temperature)
-#         next_token_logits = apply_top_k(next_token_logits, top_k) if top_k is not None else next_token_logits
-#         # next_token_logits = apply_top_p(next_token_logits, top_p) if top_p
-#         next_token = select_next_token(next_token_logits, temperature)
-#         if(tokenizer.eos_token_id is not None
-#             and next_token.item() == tokenizer.eos_token_id):
-#             break
-#         input_ids = torch.cat([input_ids, next_token], dim=-1)
-#         attention_mask = torch.cat([attention_mask, torch.ones((attention_mask.shape[0],
-#                                                                 1), device=device)], dim=-1)
-
-#     return tokenizer.decode(input_ids[0], skip_special_tokens=True)
